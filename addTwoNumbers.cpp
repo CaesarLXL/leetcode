c814914e8141f1abae205e3dc9576f044f7dd8ac
CaesarLXL <406474093@qq.com>
2018-04-10 16:34:33 +0800

    Update addTwoNumbers.cpp

--- addTwoNumbers.cpp
+++ addTwoNumbers.cpp
@@ -140,6 +140,45 @@ ListNode* Solution::addTwoNumbers(ListNode* l1, ListNode* l2) {
 	return list;
 }
 
+/*
+*method(2)-验证通过 
+*时间复杂度O(n)
+*空间复杂度O(n) 
+*/
+ListNode* Solution::addTwoNumbers(ListNode* l1, ListNode* l2) {
+	ListNode* p = l1;
+	ListNode* q = l2;
+	ListNode* list = new ListNode(0);
+	ListNode* curr = list;
+	
+	int carry = 0;
+	
+	while (p != NULL || q != NULL) {
+		int x = (p != NULL) ? p->val : 0;
+		int y = (q != NULL) ? q->val : 0;
+		int sum = x + y + carry;
+		if (sum >= 10) {
+			carry = 1;
+			curr->next = new ListNode(sum % 10);
+			curr = curr->next;
+		} else {
+			carry = 0;
+			curr->next = new ListNode(sum % 10);
+			curr = curr->next;
+		}
+		if (p != NULL) {
+			p = p->next;
+		}
+		if (q != NULL) {
+			q = q->next;
+		}
+	}
+	if (carry > 0) {
+		curr->next = new ListNode(carry);
+	}
+	return list->next;
+}
+
 int main(void) {
 	return 0;
 }
